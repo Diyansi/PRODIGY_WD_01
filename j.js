1d85067ec5fc965314acbce740c14c5e368ca818
Diyansi <161933453+Diyansi@users.noreply.github.com>
2024-05-15 14:45:24 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,46 @@
+function goToLinkedIn() {
+    var linkedinUrl = "https://www.linkedin.com/in/diyansi-chaudhari";
+    window.open(linkedinUrl, "_blank");
+}
+
+
+function goTog() {
+    var gUrl = "https://github.com/Diyansi";
+    window.open(gUrl, "_blank");
+}
+
+
+function goToe() {
+    var email = "diyansi326@gmail.com";
+        var subject = "Subject"; 
+        var body = "Message"; 
+        var mailtoUrl = "mailto:" + email + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+        window.open(mailtoUrl);
+}
+
+
+function redirectToIndex() {
+    window.location.href = "faoundation.html";
+}
+
+function brush() {
+    window.location.href = "blushes.html";
+}
+
+
+function eyeshadow() {
+    window.location.href = "eyeshadowpalettes.html";
+}
+
+function eyeliner() {
+    window.location.href = "eyeliner.html";
+}
+
+function bodylotion() {
+    window.location.href = "bodylotion.html";
+}
+
+
+function done() {
+    window.location.href = "done.html";
+}
